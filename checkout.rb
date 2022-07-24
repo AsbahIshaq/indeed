29b42e3a583cc7b3ef06bea75a7010da211e6b0b
Asbah Ishaq <asbah.ishaq@intercom.io>
2022-07-24 15:14:48 +0100

    wrong indendation due to sublime tab is corrected now

--- checkout.rb
+++ checkout.rb
@@ -1,27 +1,27 @@
 class Checkout
 
-	attr_reader :scanned_items, :promotional_rules
+  attr_reader :scanned_items, :promotional_rules
 
-	def initialize(promotional_rules)
-		@scanned_items 		 = {}
-		@promotional_rules = promotional_rules
-	end
+  def initialize(promotional_rules)
+    @scanned_items     = {}
+    @promotional_rules = promotional_rules
+  end
 
-	def scan(item)
-		code  = item.product_code
-		price = item.price
+  def scan(item)
+    code  = item.product_code
+    price = item.price
 
-		if @scanned_items[code]
-			@scanned_items[code][:quantity] = @scanned_items[code][:quantity] + 1
-		else
-			@scanned_items[code] = { quantity: 1, price: price }
-		end
-	end
+    if @scanned_items[code]
+      @scanned_items[code][:quantity] = @scanned_items[code][:quantity] + 1
+    else
+      @scanned_items[code] = { quantity: 1, price: price }
+    end
+  end
 
-	def total
-		total, final_prices = Promotion.apply_promotions(@promotional_rules, @scanned_items)
+  def total
+    total, final_prices = Promotion.apply_promotions(@promotional_rules, @scanned_items)
 
-		total
-	end
+    total
+  end
 
 end
